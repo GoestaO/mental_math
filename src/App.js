e11d27e728631985f6346f10261a61786cb620f9
Gösta Ostendorf <gostendorf@zalando.de>
2018-10-20 20:28:04 +0200

    All in one file

--- src/App.js
+++ src/App.js
@@ -1,42 +1,81 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+const {generateRandomNumber, generateProposedAnswer, calculateSum, compareNumbers} = require('./helper');
 
-
-const value1 = Math.floor(Math.random() * 100);
-const value2 = Math.floor(Math.random() * 100);
-const value3 = Math.floor(Math.random() * 100);
-const proposedAnswer = Math.floor(Math.random() * 3) + value1 + value2 + value3;
-
+// const proposedAnswer = generateProposedAnswer(value1, value2, value3);
 
 class App extends Component {
+  value1 = generateRandomNumber();
+  value2 = generateRandomNumber();
+  value3 = generateRandomNumber();
+  proposedAnswer = generateProposedAnswer(this.value1, this.value2, this.value3);
+
   state = {
     numCorrect: 0,
     numQuestions: 0,
-    
+    value1: this.value1,
+    value2: this.value2,
+    value3: this.value3,
+    proposedAnswer: this.proposedAnswer
+  };
+
+  // proposedAnswer = generateProposedAnswer(this.state.value1, this.state.value2, this.state.value3);
+
+  increaseNumQuestions() {
+    let numQuestions = this.state.numQuestions;
+    this.setState({
+      numQuestions: numQuestions + 1
+    })
   }
 
-  
+  increaseNumCorrect() {
+    let numCorrect = this.state.numCorrect;
+    this.setState({
+      numCorrect: numCorrect + 1
+    });
+  }
+
+  generateNewNumbers() {
+    this.value1 = generateRandomNumber();
+    this.value2 = generateRandomNumber();
+    this.value3 = generateRandomNumber();
+    this.proposedAnswer = generateProposedAnswer(this.value1, this.value2, this.value3);
+    this.setState({value1: this.value1, value2: this.value2, value3: this.value3, proposedAnswer: this.proposedAnswer});
+  }
+
+  onClick(e) {
+    let answer = e
+      .target
+      .value
+      .toString();
+    let sumOfValues = calculateSum(this.state.value1, this.state.value2, this.state.value3);
+    let rightAnswer = compareNumbers(this.state.proposedAnswer, sumOfValues).toString();
+    if (answer === rightAnswer) {
+      this.increaseNumCorrect()
+    }
+    this.increaseNumQuestions();
+    this.generateNewNumbers();
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">ReactND - Coding Practice</h1>
-        </header>
-        <div className="game">
-          <h2>Mental Math</h2>
-          <div className="equation">
-            <p className="text">{`${value1} + ${value2} + ${value3} = ${proposedAnswer}`}</p>
-          </div>
-          <button onClick={() => console.log('Hello')}>True</button>
-          <button>False</button>
-          <p className="text">
-            Your Score: {this.state.numCorrect}/{this.state.numQuestions}
-          </p>
+    return (<div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo"/>
+        <h1 className="App-title">ReactND - Coding Practice</h1>
+      </header>
+      <div className="game">
+        <h2>Mental Math</h2>
+        <div className="equation">
+          <p className="text">{`${this.state.value1} + ${this.state.value2} + ${this.state.value3} = ${this.state.proposedAnswer}`}</p>
         </div>
+        <button onClick={(e) => this.onClick(e)} value="true">True</button>
+        <button onClick={(e) => this.onClick(e)} value="false">False</button>
+        <p className="text">
+          Your Score: {this.state.numCorrect}/{this.state.numQuestions}
+        </p>
       </div>
-    );
+    </div>);
   }
 }
 
--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,20 @@
-const generate_random_number => {
-	return Math.floor(Math.random() * 100);
+const generateRandomNumber = () => {
+  return Math.floor(Math.random() * 100);
 }
+
+const generateProposedAnswer = (value1, value2, value3) => {
+  return Math.floor(Math.random() * 3) + value1 + value2 + value3;
+}
+
+const calculateSum = (value1, value2, value3) => {
+  return value1 + value2 + value3;
+}
+
+const compareNumbers = (number1, number2) => {
+  return number1 === number2;
+}
+
+module.exports.generateRandomNumber = generateRandomNumber;
+module.exports.generateProposedAnswer = generateProposedAnswer;
+module.exports.calculateSum = calculateSum;
+module.exports.compareNumbers = compareNumbers;
